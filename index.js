17a46bcb19381ed54415b746e18e556eb4e433a2
bleigh <bleigh@bleigh-macbookpro.roam.corp.google.com>
2015-07-09 11:18:53 -0700

    Extract Redirect into a class.

--- index.js
+++ index.js
@@ -1,70 +1,39 @@
 var isObject = require('lodash.isobject');
 var isArray = require('lodash.isarray');
-var minimatch = require('minimatch');
-var pathematics = require('pathematics');
-var slasher = require('glob-slasher');
 var isUrl = require('is-url');
-var pathToRegexp = require('path-to-regexp');
+
+var Redirect = require('./lib/redirect');
 
 module.exports = function (config) {
+  var redirects = [];
   if (isArray(config)) {
-    var redirects = config;
+    config.forEach(function(redir) {
+      redirects.push(new Redirect(redir.source, redir.destination, redir.type));
+    });
   } else if (isObject(config)) {
     // handle legacy object map format
-    var redirects = [];
     for (var source in config) {
       if (isObject(config[source])) {
-        redirects.push({
-          source: source,
-          destination: config[source].url,
-          type: config[source].status || 301
-        });
+        redirects.push(
+          new Redirect(source, config[source].url, config[source].status)
+        );
       } else {
-        redirects.push({
-          source: source,
-          destination: config[source],
-          type: 301
-        });
+        redirects.push(new Redirect(source,config[source],301));
       }
     }
   } else {
     throw new Error("Redirects provided in an unrecognized format");
   }
 
-  for (var i = 0; i < redirects.length; i++) {
-    // normalize the sources with leading slashes
-    redirects[i].source = slasher(redirects[i].source);
-    if (redirects[i].destination.indexOf(":") >= 0) {
-      redirects[i].regexpKeys = [];
-      redirects[i].regexp = pathToRegexp(redirects[i].source, redirects[i].regexpKeys);
-      redirects[i].regexpCompiled = pathToRegexp.compile(redirects[i].destination);
-    }
-  }
-
   var matcher = function(url) {
     for (var i = 0; i < redirects.length; i++) {
-      if (redirects[i].regexp) var regexpMatch = redirects[i].regexp.exec(url);
-
-      if (regexpMatch) {
-        var params = {};
-        for (var j = 0; j < redirects[i].regexpKeys.length; j++) {
-          params[redirects[i].regexpKeys[j].name] = regexpMatch[j + 1];
-        }
-        return {
-          type: redirects[i].type,
-          destination: redirects[i].regexpCompiled(params)
-        }
-      } else if (minimatch(url,redirects[i].source)) {
-        return redirects[i];
-      }
+      var result = redirects[i].test(url);
+      if (result) return result;
     }
-    return null;
   }
 
   return function (req, res, next) {
-
     var match = matcher(req.url);
-    var redirectObj;
 
     if (!match) {
       return next();
